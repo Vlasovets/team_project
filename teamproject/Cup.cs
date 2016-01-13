18472b29d41d40ace52b13bc17e4836ce80f3184
vlasovets <otorrent@mail.ru>
2016-01-13 13:09:38 +0300

    commit

---
+++ teamproject/Cup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamproject
+{
+    class Cup
+    {
+        public Team Name { get; set; }
+        public int id { get; set; }
+    }
+}
